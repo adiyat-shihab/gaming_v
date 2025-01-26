5f8a2056823d85883fb0376d2141a2ef0f6e6dae
adiyat-shihab <mohammadshihab104@gmail.com>
2025-01-26 08:51:54 +0600

    fix next config

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -10,17 +10,17 @@ const options = {};
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-// if (process.env.NODE_ENV === "development") {
-//   // Use a global variable to preserve the client during development
-//   if (!global._mongoClientPromise) {
-//     client = new MongoClient(uri, options);
-//     global._mongoClientPromise = client.connect();
-//   }
-//   clientPromise = global._mongoClientPromise;
-// } else {
-// In production, avoid using a global variable
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
-// }
+if (process.env.NODE_ENV === "development") {
+  // Use a global variable to preserve the client during development
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // In production, avoid using a global variable
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
 
 export default clientPromise;
